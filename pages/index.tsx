f8e07996fa810b1f2985b7469aae8c54f45282dc
Miroslavetsh <dfgshte@gmail.com>
2022-06-10 08:04:34 +0300

    Update title

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,15 @@ import type { NextPage } from 'next'
 import Container from '@mui/material/Container'
 
 import { ChooseTeacher } from '@components/Form'
+import Head from 'next/head'
 
 const Home: NextPage = () => {
   return (
     <Container>
+      <Head>
+        <title>Скедьюлер | Ваш Помічник</title>
+      </Head>
+
       <ChooseTeacher />
     </Container>
   )
