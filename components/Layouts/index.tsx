7ddd7b1be30d73cf834482c854d9f507733f99c8
Miroslavetsh <dfgshte@gmail.com>
2022-07-06 13:28:37 +0300

    Repair basic Layout

--- components/Layouts/index.tsx
+++ components/Layouts/index.tsx
@@ -1,20 +1,14 @@
 import Container from '@mui/material/Container'
 
-import FlipButton from '@components/FlipButton'
-
 type PropTypes = {
   children: React.ReactNode
 }
 
 const Layout: React.FC<PropTypes> = ({ children }) => {
   return (
-    <>
-      <main>
-        <Container sx={{ mt: 2 }}>{children}</Container>
-      </main>
-
-      <FlipButton onClick={() => {}} frontContent='Є аккаунт?' backContent='Увійти' />
-    </>
+    <main>
+      <Container sx={{ mt: 2 }}>{children}</Container>
+    </main>
   )
 }
 
