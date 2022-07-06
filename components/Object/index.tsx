4a24685ed2445c51787fdbacee0c7e4b085a1542
Miroslavetsh <dfgshte@gmail.com>
2022-07-06 13:46:54 +0300

    Refactor ObjectTemplate

--- components/Object/index.tsx
+++ components/Object/index.tsx
@@ -1,27 +1,30 @@
 import { Button } from '@mui/material'
 
-import Entity from '@models/Entity'
 import Link from '@components/NextLink'
-import deleteItemFromAPI from '@src/utils/services/deleteItemFromAPI'
-import curry from '@src/utils/curry'
-import TableName from '@src/utils/TableName'
 
-type ObjectTemplateProps<T extends Entity> = { entity: T, tableName: TableName }
+import Entity from '@models/Entity'
 
-const ObjectTemplate = <T extends Entity>({ entity, tableName }: ObjectTemplateProps<T>): JSX.Element => {
+import deleteItemFromAPI from '@utils/services/deleteItemFromAPI'
+import curry from '@utils/curry'
+import TableName from '@utils/TableName'
+
+type ObjectTemplateProps<T extends Entity> = { entity: T; tableName: TableName }
+
+const ObjectTemplate = <T extends Entity>({
+  entity,
+  tableName,
+}: ObjectTemplateProps<T>): JSX.Element => {
   const deleteEntity = () => {
     curry<string>(deleteItemFromAPI)(tableName, entity.id)
   }
 
   return (
     <ul>
-      {Object.keys(entity).map(
-        (field) => {
-          // @ts-ignore
-          if (field !== 'id') return <li key={field.toString()}>{entity[field]}</li>
-          if (field.includes('Id')) return <li key={field.toString()}>{entity[field]}</li>
-        },
-      )}
+      {Object.keys(entity).map((field) => {
+        // @ts-ignore
+        if (field !== 'id') return <li key={field.toString()}>{entity[field]}</li>
+        if (field.includes('Id')) return <li key={field.toString()}>{entity[field]}</li>
+      })}
 
       <Link href={`${tableName}/edit/${entity.id}`}>edit</Link>
       <Button onClick={deleteEntity}>delete</Button>
