5cc5c196c0ab6214eccaa0ce0e4defa9600cd9a5
Miroslavetsh <dfgshte@gmail.com>
2022-07-19 15:27:53 +0300

    Remove unneeded resources

--- src/pages/api/teachers.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import TeacherController from '@controllers/Teacher'
-
-type Data = Array<number>
-
-export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  TeacherController.getAll().then((data) => res.status(200).json(data as Data))
-}
