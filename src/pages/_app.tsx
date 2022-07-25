6cb9c69bc045b6c7f34592a6408c080a5695b92a
Miroslavetsh <dfgshte@gmail.com>
2022-07-25 11:55:58 +0300

    Remove next-auth from deps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,6 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { CacheProvider, EmotionCache } from '@emotion/react'
-import type { SessionProviderProps } from 'next-auth/react'
-import { SessionProvider } from 'next-auth/react'
 import CssBaseline from '@mui/material/CssBaseline'
 
 import createEmotionCache from '@src/utils/createEmotionCache'
@@ -10,33 +8,25 @@ import Layout from '@src/components/Layouts'
 
 import '../styles/globals.scss'
 
-type PropTypes = AppProps &
-  SessionProviderProps & {
-    emotionCache?: EmotionCache
-  }
+type PropTypes = AppProps & {
+  emotionCache?: EmotionCache
+}
 
 const clientSideEmotionCache = createEmotionCache()
 
-const MyApp = ({
-  Component,
-  emotionCache = clientSideEmotionCache,
-  pageProps,
-  session,
-}: PropTypes) => {
+const MyApp = ({ Component, emotionCache = clientSideEmotionCache, pageProps }: PropTypes) => {
   return (
-    <SessionProvider session={session}>
-      <CacheProvider value={emotionCache}>
-        <Head>
-          <meta name='viewport' content='initial-scale=1, width=device-width' />
-        </Head>
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name='viewport' content='initial-scale=1, width=device-width' />
+      </Head>
 
-        <CssBaseline />
+      <CssBaseline />
 
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </CacheProvider>
-    </SessionProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CacheProvider>
   )
 }
 
